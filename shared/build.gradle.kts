98b139dee628d827b3329ec8b914dd73fa7bd48e
StanHigh <stasntnew@gmail.com>
2024-05-06 19:18:50 +0200

    initial commit

---
+++ shared/build.gradle.kts
@@ -0,0 +1,65 @@
+import com.android.utils.cxx.os.createWindowsBehavior
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.sqldelight)
+}
+
+kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            //put your multiplatform dependencies here
+            implementation(libs.runtime)
+            implementation(libs.kotlinx.datetime)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+        androidMain.dependencies {
+            implementation(libs.android.driver)
+        }
+        iosMain.dependencies {
+            implementation(libs.native.driver)
+        }
+    }
+}
+
+sqldelight{
+    databases{
+        create("NoteDatabase"){
+            packageName.set("id.slava.nt.myfirstkmmapp.database")
+        }
+    }
+}
+
+android {
+    namespace = "id.slava.nt.myfirstkmmapp"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
